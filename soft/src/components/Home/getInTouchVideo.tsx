67281b916e2b9dd38f65c5410c0fda30c9a77340
Ethan <ethan25805@gmail.com>
2025-01-08 18:01:06 +1100

    Implement VideoDialog for 'Get In Touch Button' : Placeholder Video used

---
+++ soft/src/components/Home/getInTouchVideo.tsx
@@ -0,0 +1,20 @@
+import {HeroVideoDialog} from "../ui/videoDialog";
+
+interface VideoDialogProps {
+    button: React.ReactElement;
+  }
+
+export function VideoDialog({button}:VideoDialogProps) {
+    console.log("Button received in VideoDialog: ", button);
+  return (
+    <div className="relative">
+      <HeroVideoDialog
+        animationStyle="from-center"
+        videoSrc="https://www.youtube.com/embed/PmrkY-EZy74"
+        thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
+        thumbnailAlt="Hero Video"
+        button = {button}
+      />
+    </div>
+  );
+}
