1be5255cd06f9a447174b9449e5a1b94e2922314
Ethan <ethan25805@gmail.com>
2025-02-14 14:00:22 +1100

    Add Features: mouse tracking, state reset, and onClose handler

--- soft/src/components/ui/dialog.tsx
+++ soft/src/components/ui/dialog.tsx
@@ -7,7 +7,10 @@ import { X } from "lucide-react"
 import { cn } from "../../utils/cn"
 
 interface DialogContentProps extends React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> {
-  isTracking?: React.Dispatch<React.SetStateAction<boolean>>; // Add your custom prop
+  isMouseTracking?: React.Dispatch<React.SetStateAction<boolean>>; // Add your custom prop
+  isTryNowTriggered?: boolean;
+  updateIsTryNowTriggered?: React.Dispatch<React.SetStateAction<boolean>>;
+  onClose?: any;
 }
 
 const Dialog = DialogPrimitive.Root
@@ -21,7 +24,7 @@ const DialogClose = DialogPrimitive.Close
 const DialogOverlay = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Overlay>,
   DialogContentProps
->(({ className, isTracking, ...props }, ref) => (
+>(({ className, isMouseTracking,  isTryNowTriggered, updateIsTryNowTriggered, ...props }, ref) => (
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
@@ -30,8 +33,11 @@ const DialogOverlay = React.forwardRef<
     )}
     {...props}
     onClick = {() => {
-      if (isTracking) {
-        isTracking(true);
+      if (isMouseTracking) {
+        isMouseTracking(true);
+      }
+      if (isTryNowTriggered && updateIsTryNowTriggered) {
+        updateIsTryNowTriggered(false);
       }
     }}
   />
@@ -41,13 +47,13 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   DialogContentProps
->(({ className, children, isTracking, ...props }, ref) => (
+>(({ className, children, onClose, isMouseTracking, isTryNowTriggered, updateIsTryNowTriggered, ...props }, ref) => (
   <DialogPortal>
-    <DialogOverlay isTracking={isTracking} />
+    <DialogOverlay onClose = {onClose} isMouseTracking={isMouseTracking} isTryNowTriggered={isTryNowTriggered} updateIsTryNowTriggered={updateIsTryNowTriggered} />
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-slate-50 p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+        "fixed left-[50%] top-[50%] z-50 grid translate-x-[-50%] translate-y-[-50%] gap-4 border bg-slate-50 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
         className
       )}
       {...props}
@@ -55,9 +61,15 @@ const DialogContent = React.forwardRef<
       {children}
       <DialogPrimitive.Close  
           onClick = {() => {
-            if (isTracking) {
-            isTracking(true);
+            if (isMouseTracking) {
+              isMouseTracking(true);
 
+            }
+            if (isTryNowTriggered && updateIsTryNowTriggered) {
+              updateIsTryNowTriggered(false);
+            }
+            if (onClose) {
+              onClose();
             }
           }} 
           className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
